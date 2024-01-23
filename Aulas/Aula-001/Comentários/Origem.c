483a2911e46f44da66d9fa965300e4a75bcf36fe
macielthiago <maciel.rosari54@gmail.com>
2024-01-23 18:13:50 -0400

    introdução ao C

---
+++ Aulas/Aula-001/Comentários/Origem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+	SetConsoleCP(1252);
+	SetConsoleOutputCP(1252);
+
+	/*
+		Isso aqui 
+		é um grande bloco 
+		de comentário
+	*/
+
+	printf("Aprendendo comentários na linguagem C. \n");
+	//printf("Hello world! \n");
+
+	return 0;
+}
