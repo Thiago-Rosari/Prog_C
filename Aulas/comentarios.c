8a845980465cec5f45360e17e7d350cdf8faca03
macielthiago <maciel.rosari54@gmail.com>
2024-06-05 18:16:36 -0400

    Comentários  em C

---
+++ Aulas/comentarios.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Aprendendo comentarios na linguagem C\n");
+
+    // printf("Hello World! \n");
+
+    /*
+        bloco de comentário
+    */
+    return 0;
+}
