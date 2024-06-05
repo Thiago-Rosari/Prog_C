0818980c16a7f255c7cde633d2fcaf70f50a456e
macielthiago <maciel.rosari54@gmail.com>
2024-06-05 18:47:38 -0400

    Função printf

---
+++ Aulas/Aula001/imprimir.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // printf é uma função de saída
+    printf("Ola. Seja bem vindo! \n");
+
+    printf("\n---------------------------------------\n\n");
+    printf("1 - Logar  2 - Cadastrar  3 - Imprimir\n");
+    printf("\n---------------------------------------\n\n");
+
+    printf("\nValor retornado: %d", printf("Bom"));
+
+    return 0;
+}
