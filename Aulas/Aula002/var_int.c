e47d4c04a8af4fb5f503c8963c8c7bb29ac0d3d9
macielthiago <maciel.rosari54@gmail.com>
2024-06-06 18:38:17 -0400

    Variáveis

---
+++ Aulas/Aula002/var_int.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor, valor2;
+    //valor = 50;
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &valor);
+
+    printf("Digite um segundo valor inteiro: ");
+    scanf("%d", &valor2);
+
+    printf("Primeiro valor: %d \nSegundo valor: %d\n", valor, valor2);
+
+
+
+    return 0;
+}
---
+++ Aulas/Aula002/var_real.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num = 3.1415;
+
+    printf("Valor da minha variavel: %.4f\n", num);
+
+    printf("Digite um numero real: ");
+    scanf("%f", &num);
+
+    printf("Valor digitado: %.3f", num);
+
+    return 0;
+}
