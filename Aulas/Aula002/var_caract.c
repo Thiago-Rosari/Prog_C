bfbf9f4ef0fd1b49f2c2219d952c254c61f60905
macielthiago <maciel.rosari54@gmail.com>
2024-06-07 18:30:13 -0400

    Tipo caractere

---
+++ Aulas/Aula002/var_caract.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char sexo = 'M';
+
+    printf("Valor da variavel sexo: %c \n", sexo);
+
+    printf("Digite seu sexo: (F,M)\n");
+    scanf("%c", &sexo);
+
+    printf("Valor da variavel sexo: %c\n", sexo);
+
+    return 0;
+}
