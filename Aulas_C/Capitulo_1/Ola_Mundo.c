30f09a73c3dc93b82f2379a2fc5ec63fb41b0762
macielthiago <maciel.rosari54@gmail.com>
2024-11-24 08:39:02 -0400

    Olá, Mundo_C

---
+++ Aulas_C/Capitulo_1/Ola_Mundo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // comentários de uma linha iniciam com // (duas barras)
+
+    // a função printf direciona o texto inserido (entre aspas duplas)
+    // para a saída padrão
+
+    printf("Hello, World!!");
+    
+    return 0;
+}
