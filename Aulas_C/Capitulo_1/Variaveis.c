6da507ab8699513d43e3482193edb7500bf8bb26
macielthiago <maciel.rosari54@gmail.com>
2024-11-24 09:09:12 -0400

    Variáveis

---
+++ Aulas_C/Capitulo_1/Variaveis.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // Cada variável precia de um tipo e de um identificador (nome)
+    //
+    //  Os tipos que serão usados por enquanto são:
+    //     int: número inteiro
+    //     char: caractere
+    //     float: número decial/ponto flutuante
+    
+    int idade;                 // variável inteira vhamada idade
+    char letra;                // variável para caracteres chaada letra
+    float altura;              // variável decimal chamada altura
+
+    int ParesDeTenis = 5;      // variável inteira chamada ParesDeTenis
+                               // inicializada com valor 5
+
+    char letraInicial = 'D';   // variável para caractere chamada letrraInicial
+                               // inicializada com valor 'D'
+
+    float peso = 90.5;         // variável decimal chamada peso 
+                               // inicializada com valor 90.5
+                               // use (ponto) para separar número decimal
+    return 0;
+}
