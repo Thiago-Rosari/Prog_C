d62d87f8274bc0685759cac1f5b2cd6b1c900f23
macielthiago <maciel.rosari54@gmail.com>
2024-11-24 08:47:22 -0400

    Comentários

---
+++ Aulas_C/Capitulo_1/Comentarios.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    /*
+        comentário de múltilas linhas
+    */
+    //  comentário de linha punica
+    //  \n -> caractere de pular linha
+    //  \t -> caractere de tabulação
+
+    printf("Thiago\n");
+    printf("Maciel");
+    printf("\tdo Rosario");
+
+    return 0;
+}
