392d0abc72c4c56cb5c67df4515e65fa38d8b690
macielthiago <maciel.rosari54@gmail.com>
2024-12-08 18:40:55 -0400

    Estrutura if

---
+++ Aulas_C/Capitulo_2/Estrutura_if.c
@@ -0,0 +1,24 @@
+/*
+* Arquivo: EstruturasCondicionaisOperadorTernarioIf.c
+* Autor: Prof. Dr. David Buzatto
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+    int n = 20;
+    int v;
+
+    // n é par? se sim, atribui 1 a v, caso contrário, atribui 0
+    if (n % 2 == 0) {
+        v = 1;
+    } else{
+        v = 0;
+    }
+    
+    printf("%d", v);
+
+    return 0;
+}
